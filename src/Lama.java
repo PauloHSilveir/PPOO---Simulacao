7246b40d41a4937384b1473804715f035d9093ff
Gabriel Jardim <gabriel.souza16@estudante.ufla.br>
2025-01-27 19:35:02 -0300

    Adicionando background e alterando as dimensoes

--- src/Lama.java
+++ src/Lama.java
@@ -9,7 +9,7 @@ public class Lama extends Obstaculo {
         if (formiga.getLocalizacao().equals(getLocalizacao())) {
             System.out.println("A formiga entrou na lama e está mais lenta!");
             // Reduz a velocidade da formiga atrasando o próximo movimento
-            formiga.setVelocidade(formiga.getVelocidade() + 500);
+            //formiga.setVelocidade(formiga.getVelocidade() + 500);
         }
     }
 }
--- src/Tamandua.java
+++ src/Tamandua.java
@@ -8,7 +8,7 @@ public class Tamandua extends Obstaculo {
     public void afetarFormiga(Formiga formiga) {
         if (formiga.getLocalizacao().equals(super.getLocalizacao())) {
             System.out.println("A formiga encontrou um tamanduá e foi atrasada!");
-            formiga.setVelocidade(formiga.getVelocidade() + 500); // Exemplo: reduz a velocidade pela metade
+            //formiga.setVelocidade(formiga.getVelocidade() + 500); // Exemplo: reduz a velocidade pela metade
             // Faz a formiga permanecer na posição atual (parada) por um turno
             formiga.setLocalizacaoDestino(formiga.getLocalizacao());
         }
